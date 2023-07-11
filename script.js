f90dd01d3b7db641524534fdd32a822a3f6edfa7
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-07-11 13:56:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.getElementById("sum_btn").addEventListener("click", function() {
+      var num1 = parseFloat(document.getElementById("n1").value);
+      var num2 = parseFloat(document.getElementById("n2").value);
+      var sum = num1 + num2;
+      document.getElementById("sum").textContent = "Sum: " + sum;
+    });
